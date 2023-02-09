8eb657686e50aa4a05050fa49f8103817cc1d911
bojovic-a <abojovic68@gmail.com>
2023-02-09 16:52:28 +0100

    First try inserting questions, incomplete login system(add session contol)

--- index.js
+++ index.js
@@ -3,6 +3,26 @@ var app = express();
 var pug = require('pug');
 var bodyParser = require('body-parser');
 var mysql = require('mysql')
+const crypto = require('crypto');
+const session = require('express-session');
+var cookieParser = require('cookie-parser')
+
+app.use(cookieParser());
+
+app.use(function(req,res,next){
+    res.locals.session = req.session;
+    next();
+});
+
+app.use(session({
+    secret: 'VERY_HARD_TO_CRACK_KEY',
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 var db = mysql.createConnection({
     database: "jsurvey_db",
@@ -28,50 +48,132 @@ var CompiledRegister = pug.compileFile('./templates/register.pug');
 var CompiledMakeASurvey = pug.compileFile('./templates/make_a_survey.pug');
 
 app.get('/', (req, resp) => {
-    resp.send(CompiledIndex());
+    console.log(session)
+    resp.send(CompiledIndex());             
 });
 
 app.get('/login', (req, resp) => {
     resp.send(CompiledLogin());
 });
 
+app.post('/login', (req, resp) => {
+
+    username = req.body.username
+    password = req.body.password    
+    
+    hash = crypto.createHash("sha256")
+    hash.update(password)
+    hashed_pass = hash.digest("hex")
+
+    sql = "SELECT * FROM user WHERE username = ?"
+    db.query(sql, [username], (err, res) => {
+        if (err) throw err;
+
+        if (res.length > 0) {
+            if (hashed_pass == res[0].password){
+                session.username = res[0].username
+                session.user_id = res[0].user_id
+                resp.send(CompiledIndex())
+            }
+
+            else {
+                resp.send(CompiledLogin({
+                    error: "Wrong password"
+                }))
+            }
+        } 
+        
+        else {
+            resp.send(CompiledLogin({
+                error: "Enter your credentials"
+            }))
+        }
+    })
+
+})
+
 app.get('/register', (req, resp) => {
     resp.send(CompiledRegister());
 });
 
+app.post('/register', (req, resp) => {
+    username = req.body.username
+    email = req.body.email
+    password = req.body.password
+
+    hash = crypto.createHash("sha256")
+    hash.update(password)
+    hashed_pass = hash.digest("hex")
+
+    console.log(hashed_pass)
+
+    sql = "INSERT INTO user VALUES (null, ?, ?, ?)"
+
+    db.query(sql, [email, username, hashed_pass], (err, res) => {
+        if (err) throw err;
+        resp.redirect('/login')
+    })
+})
+
+app.get('/logout', (req, resp) => {
+    req.session.destroy()
+    resp.redirect('/login')
+})
+
 app.get('/make-a-survey', (req, resp) => {
-    resp.send(CompiledMakeASurvey());
-});
 
-app.post('/add_question', (req, resp) => {
-    console.log(req.body)
-    question_text = req.body['question-text'];
-    if ("radio-option" in req.body){
-        answer_options = req.body['radio-option'];
-    }
-    if ("checkbox-option" in req.body){
-        answer_options = req.body.radio['checkbox-option'];
+    if (!session.user_id) {
+        resp.send(CompiledLogin({
+            error: "You have to be logged in to post surveys"
+        }))
     }
 
-    user_id = 1      
+    user_id_session = session.user_id.toString()
     let date = new Date() 
     let day = date.getDate();
     let month = date.getMonth() + 1;
     let year = date.getFullYear();
-    
     let current_date_format = `${day}-${month}-${year}`;
 
+    sql = "INSERT INTO survey VALUES(null, ?, ?)"
+
+    db.query(sql, [user_id_session, current_date_format], (err, res) => {
+        if (err) throw err;
+
+        resp.send(CompiledMakeASurvey());
+    })    
+});
+
+app.post('/add_question', (req, resp) => {    
+    question_text = req.body['question-text'];
+    var question_type, answer_options;
+    if ("radio-option" in req.body){
+        answer_options = req.body['radio-option'];
+        question_type = "radio"
+    }
+    if ("checkbox-option" in req.body){
+        question_type = "checkbox"
+        answer_options = req.body.radio['checkbox-option'];
+    }   
+        
+    
     sql = `
-        CALL spInsertSurvey(null, ?, ?)        
+        INSERT INTO question VALUES(null, ?, ?, ?)
     `
-    db.query(sql, [user_id, current_date_format], (err, res) => {
-        if(err) throw err;
-        console.log("Uspesno dodato" + result)
-        resp.redirect("/make-a-survey")
-    })
-    
-    
-    // NEKAKO RESI DODAVANJE ANKETE, PITANJA I ODGOVORA ODJEDNOM
+    console.log(survey_id,question_text, question_type)
+    db.query(sql, [survey_id,question_text,question_type], (err, res) => {
+        if (err) throw err;
+        
+        question_id = res['insertId']
+        sql = "INSERT INTO answer VALUES (null, ?, ?)"
+
+        for (const answer of answer_options) {
+            db.query(sql, [question_id, answer], (err, res) => {
+                if (err) throw err;
+                resp.send(CompiledMakeASurvey())
+            })
+        }
+    })    
     
 
 })
