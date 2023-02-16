fe91e6a1424d0361f560f8f4587057d8cfa11e62
bojovic-a <abojovic68@gmail.com>
2023-02-17 00:40:05 +0100

    version

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var app = express();
-var pug = require('pug');
-var bodyParser = require('body-parser');
-var mysql = require('mysql')
+const express = require('express');
+const app = express();
+const pug = require('pug');
+const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const session = require('express-session');
-var cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const { type } = require('os');
+const mongoose = require("mongoose")
+
+mongoose.set("strictQuery", false)
+mongoose.connect("mongodb://localhost:5000/jsurvey_db")
 
 app.use(cookieParser());
 
@@ -27,19 +30,8 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json())
 
-var db = mysql.createConnection({
-    database: "jsurvey_db",
-    host : "localhost",
-    user : "root",
-    password : ""
-})
-
-db.connect((err) => {
-    if (err) console.log(err);
-    console.log("Connected to database")
-})
-
 app.use(express.static('./static'));
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -51,8 +43,8 @@ var CompiledRegister = pug.compileFile('./templates/register.pug');
 var CompiledMakeASurvey = pug.compileFile('./templates/make_a_survey.pug');
 var CompiledAddSurveyName = pug.compileFile('./templates/add_survey_name.pug')
 
-app.get('/', (req, resp) => {
-    console.log(req.session)
+
+app.get('/', (req, resp) => {    
     resp.send(CompiledIndex({
         user: req.session.username,
         user_id: req.session.user_id
@@ -66,51 +58,51 @@ app.get('/login', (req, resp) => {
     }));
 });
 
-app.post('/login', (req, resp) => {
+// app.post('/login', (req, resp) => {
 
-    username = req.body.username
-    password = req.body.password    
+//     username = req.body.username
+//     password = req.body.password    
     
-    hash = crypto.createHash("sha256")
-    hash.update(password)
-    hashed_pass = hash.digest("hex")
+//     hash = crypto.createHash("sha256")
+//     hash.update(password)
+//     hashed_pass = hash.digest("hex")
 
-    sql = "SELECT * FROM user WHERE username = ?"
-    db.query(sql, [username], (err, res) => {
-        if (err) throw err;
+//     sql = "SELECT * FROM user WHERE username = ?"
+//     db.query(sql, [username], (err, res) => {
+//         if (err) throw err;
 
-        if (res.length > 0) {
-            if (hashed_pass == res[0].password){
+//         if (res.length > 0) {
+//             if (hashed_pass == res[0].password){
                 
-                req.session.username = res[0].username
-                req.session.user_id = res[0].user_id 
-                console.log(res[0].user_id)
-                console.log("User id iz /login: " + res[0].user_id)
-                resp.send(CompiledIndex({
-                    user: req.session.username,
-                    user_id: req.session.user_id
-                }))
-            }
+//                 req.session.username = res[0].username
+//                 req.session.user_id = res[0].user_id 
+//                 console.log(res[0].user_id)
+//                 console.log("User id iz /login: " + res[0].user_id)
+//                 resp.send(CompiledIndex({
+//                     user: req.session.username,
+//                     user_id: req.session.user_id
+//                 }))
+//             }
 
-            else {
-                resp.send(CompiledLogin({
-                    user: req.session.username,
-                    user_id: req.session.user_id,
-                    error: "Wrong password"
-                }))
-            }
-        } 
+//             else {
+//                 resp.send(CompiledLogin({
+//                     user: req.session.username,
+//                     user_id: req.session.user_id,
+//                     error: "Wrong password"
+//                 }))
+//             }
+//         } 
         
-        else {
-            resp.send(CompiledLogin({
-                user: req.session.username,
-                user_id: req.session.user_id,
-                error: "Enter your credentials"
-            }))
-        }
-    })
+//         else {
+//             resp.send(CompiledLogin({
+//                 user: req.session.username,
+//                 user_id: req.session.user_id,
+//                 error: "Enter your credentials"
+//             }))
+//         }
+//     })
 
-})
+// })
 
 app.get('/register', (req, resp) => {
     resp.send(CompiledRegister({
@@ -119,24 +111,36 @@ app.get('/register', (req, resp) => {
     }));
 });
 
-app.post('/register', (req, resp) => {
-    username = req.body.username
-    email = req.body.email
-    password = req.body.password
+// app.post('/register', (req, resp) => {
+//     username = req.body.username
+//     email = req.body.email
+//     password = req.body.password
 
-    hash = crypto.createHash("sha256")
-    hash.update(password)
-    hashed_pass = hash.digest("hex")
+//     hash = crypto.createHash("sha256")
+//     hash.update(password)
+//     hashed_pass = hash.digest("hex")
 
-    console.log(hashed_pass)
+//     existing_user = getUserByUsername(username)
+//     console.log(existing_user)
+//     if (existing_user) {
+//         resp.send(CompiledRegister({
+//             user: req.session.username,
+//             user_id: req.session.user_id,
+//             error: "User already exists"
+//         }))
+//         return
+//     }
 
-    sql = "INSERT INTO user VALUES (null, ?, ?, ?)"
+//     console.log(hashed_pass)
+//     newUser = {
+//         username: username,
+//         email: email,
+//         password: hashed_pass
+//     }
+//     insertUser(newUser)
+//     resp.redirect('/')
 
-    db.query(sql, [email, username, hashed_pass], (err, res) => {
-        if (err) throw err;
-        resp.redirect('/login')
-    })
-})
+// })
 
 app.get('/logout', (req, resp) => {
     req.session.destroy()
@@ -144,37 +148,37 @@ app.get('/logout', (req, resp) => {
 })
 
 app.get('/make-a-survey', (req, resp) => {
-    if (!req.session.username) {
-        resp.redirect('/login')
-        return
-    }
+    // if (!req.session.username) {
+    //     resp.redirect('/login')
+    //     return
+    // }
     resp.send(CompiledAddSurveyName({
         user: req.session.username,
         user_id: req.session.user_id
     }))
 })
 
-app.post('/proceed_to_survey', (req, resp) => {
+// app.post('/proceed_to_survey', (req, resp) => {
     
-    survey_name = req.body['survey-name']
-    survey_description = req.body['survey-description']
+//     survey_name = req.body['survey-name']
+//     survey_description = req.body['survey-description']
 
-    req.session["survey_name"] = survey_name
-    req.session["survey_description"] = survey_description
+//     req.session["survey_name"] = survey_name
+//     req.session["survey_description"] = survey_description
 
-    resp.send(CompiledMakeASurvey({
-        title: survey_name,
-        description: survey_description
-    }))
-})
+//     resp.send(CompiledMakeASurvey({
+//         title: survey_name,
+//         description: survey_description
+//     }))
+// })
 
-app.post('/save-survey', (req, resp) => {
-    questions = req.body.all_questions    
-    title = req.session.survey_name
-    description = req.session.survey_description
-    console.log(title, description, questions)
+// app.post('/save-survey', (req, resp) => {
+//     questions = req.body.all_questions    
+//     title = req.session.survey_name
+//     description = req.session.survey_description
+//     console.log(title, description, questions)
 
-})
+// })
 
 
 app.listen(process.env.PORT || 5000, () => console.log("App is being served on: http://localhost:5000"));
