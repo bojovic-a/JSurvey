2a6e2a090c47d9bf08c0e88dfc43f365f3d4ed87
bojovic-a <abojovic68@gmail.com>
2023-02-23 02:55:17 +0100

    Anwer input type

---
+++ static/scripts/fillin.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', init)
+
+function init(){
+    var submit_button = document.getElementById("submit-form")
+    submit_button.addEventListener("click", get_form_data)    
+}
+
+function get_form_data() {
+    // var all_forms = document.getElementsByClassName("question-answer-form")[0]
+    // console.log(all_forms)
+    console.log("Nja")
+}
